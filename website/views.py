4af35fa69e08354dd3fe2b3bc21a919a652f7bcb
Abhijith Chandran <abhijith77chandran@gmail.com>
2025-06-24 22:10:12 +0530

    Switched doctor list to use Doctordetails model

--- website/views.py
+++ website/views.py
@@ -95,7 +95,8 @@ def feature(request):
     return render(request, 'feature.html')
 def team(request):
     departments = Department.objects.all() 
-    doctors = Doctor.objects.all()
+    doctors = Doctordetails.objects.all()
+
     therapy = Therapist.objects.all()
     blog = Blog.objects.all()
     # Fetch all doctors from the database
