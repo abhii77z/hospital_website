7ca371a45c3521ce41e52bb35a8749c89a0e49f7
Abhijith Chandran <abhijith77chandran@gmail.com>
2025-06-24 15:57:46 +0530

    Fix: added missing path/include import to urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
+from django.urls import path, include  # ✅ You missed importing 'path' and 'include'
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -22,6 +24,6 @@ urlpatterns = [
     path('', include('website.urls')),
 ]
 
-# Always serve media files (even in production)
+# Serve media files (uploads like doctor images)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
